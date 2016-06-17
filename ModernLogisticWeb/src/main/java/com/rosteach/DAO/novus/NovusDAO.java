3d0a0b5c1e9dfa660becbfd046dde32acbb4e3ef
koval ihor <kovalihorsmail@gmail.com>
2016-06-17 14:52:55 +0300

    novus last

--- ModernLogisticWeb/src/main/java/com/rosteach/DAO/novus/NovusDAO.java
+++ ModernLogisticWeb/src/main/java/com/rosteach/DAO/novus/NovusDAO.java
@@ -1,7 +1,8 @@
 package com.rosteach.DAO.novus;
 
+import java.sql.SQLException;
 
 public interface NovusDAO {
-public String test();
-int Insert(String database, String name, String password, String path);
+public String test() throws SQLException;
+String Insert(String database, String name, String password, String path) throws SQLException;
 }
